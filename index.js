7e42310bad5704f97896781f6611e7d9a4e98094
ankush <ankushsolanki3287@gmail.com>
2022-07-05 22:48:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let ext = path.extname(filePath);
+  let contentType = "text/html";
+  switch (ext) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server Running on port:${PORT}`));
